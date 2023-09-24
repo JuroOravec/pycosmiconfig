dac2d6be5d5242cd0d941658516fcc26e8c744c7
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-09-25 00:01:10 +0200

    chore: pass args via poetry scripts

--- manage.py
+++ manage.py
@@ -1,25 +1,30 @@
+import sys
 import subprocess
 
 
+def _parse_args():
+    return sys.argv[1:] or []
+
+
 def run_format():
-    subprocess.run(["bash", "scripts/format.sh"])
+    subprocess.run(["bash", "scripts/format.sh", *_parse_args()])
 
 
 def run_format_imports():
-    subprocess.run(["bash", "scripts/format-imports.sh"])
+    subprocess.run(["bash", "scripts/format-imports.sh", *_parse_args()])
 
 
 def run_lint():
-    subprocess.run(["bash", "scripts/lint.sh"])
+    subprocess.run(["bash", "scripts/lint.sh", *_parse_args()])
 
 
 def run_test():
-    subprocess.run(["bash", "scripts/test.sh"])
+    subprocess.run(["bash", "scripts/test.sh", *_parse_args()])
 
 
 def run_test_cov():
-    subprocess.run(["bash", "scripts/test-cov.sh"])
+    subprocess.run(["bash", "scripts/test-cov.sh", *_parse_args()])
 
 
 def run_test_cov_html():
-    subprocess.run(["bash", "scripts/test-cov-html.sh"])
+    subprocess.run(["bash", "scripts/test-cov-html.sh", *_parse_args()])
